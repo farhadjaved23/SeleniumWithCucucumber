1912fb514f6308e8afbff979a9f49a0e0d85a55b
farhadjaved23 <47040241+farhadjaved23@users.noreply.github.com>
2019-01-31 20:24:11 +0500

    updated
    updated

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -31,7 +31,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\FARHAD\\Download\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\FARHAD\\Downloads\\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
